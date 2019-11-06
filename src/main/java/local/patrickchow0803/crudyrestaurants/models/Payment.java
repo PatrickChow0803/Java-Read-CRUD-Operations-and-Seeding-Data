99b3a42790084c4fc8162a4ce7cddf026c85fc44
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-06 13:16:40 -0500

    Finished All the Models

--- src/main/java/local/patrickchow0803/crudyrestaurants/models/Payment.java
+++ src/main/java/local/patrickchow0803/crudyrestaurants/models/Payment.java
@@ -1,6 +1,8 @@
 package local.patrickchow0803.crudyrestaurants.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "payments")
@@ -14,4 +16,39 @@ public class Payment {
     @Column(nullable = false,
             unique = true)
     private String type;
+
+    @ManyToMany(mappedBy = "payments")
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public Payment(){
+        // Spring Data JPA Requires this default constructor
+    }
+
+    public Payment(String type) {
+        this.type = type;
+    }
+
+    public long getPaymentid() {
+        return paymentid;
+    }
+
+    public void setPaymentid(long paymentid) {
+        this.paymentid = paymentid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
 }
--- src/main/java/local/patrickchow0803/crudyrestaurants/models/Restaurant.java
+++ src/main/java/local/patrickchow0803/crudyrestaurants/models/Restaurant.java
@@ -37,4 +37,89 @@ public class Restaurant {
                inverseJoinColumns = @JoinColumn(name = "paymentid") //paymentid comes from the payment model class
               )
     List<Payment> payments = new ArrayList<>();
+
+    public Restaurant(){
+        // Spring Data JPA Requires this default constructor
+    }
+
+    public Restaurant(String name, String address, String city, String state, String telephone, int seatcapacity) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.telephone = telephone;
+        this.seatcapacity = seatcapacity;
+    }
+
+    public long getRestaurantid() {
+        return restaurantid;
+    }
+
+    public void setRestaurantid(long restaurantid) {
+        this.restaurantid = restaurantid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getSeatcapacity() {
+        return seatcapacity;
+    }
+
+    public void setSeatcapacity(int seatcapacity) {
+        this.seatcapacity = seatcapacity;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
 }
