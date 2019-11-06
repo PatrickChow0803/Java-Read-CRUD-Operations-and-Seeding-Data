3279a4e1c96731db25828270c5c019c6efc817f4
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-06 12:58:11 -0500

    Added Menu Model

--- src/main/java/local/patrickchow0803/crudyrestaurants/models/Menu.java
+++ src/main/java/local/patrickchow0803/crudyrestaurants/models/Menu.java
@@ -1,4 +1,18 @@
 package local.patrickchow0803.crudyrestaurants.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "menus")
 public class Menu {
+
+    @Id // Makes this a primary key
+    @GeneratedValue(strategy = GenerationType.AUTO)// Generates the values automatically
+    private long menuid;
+
+    //Makes sure that all dishes are unique
+    @Column(nullable = true)
+    private String dish;
+    private double price;
+
 }
