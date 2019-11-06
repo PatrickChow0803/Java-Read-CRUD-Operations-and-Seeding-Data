2dd9eee72c3dd87871206d4afaf57b367cdacbef
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-06 12:27:20 -0500

    Patrick Chow: Initial Commit

---
+++ src/main/java/local/patrickchow0803/crudyrestaurants/CrudyRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package local.patrickchow0803.crudyrestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudyRestaurantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudyRestaurantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/patrickchow0803/crudyrestaurants/CrudyRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package local.patrickchow0803.crudyrestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyRestaurantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
