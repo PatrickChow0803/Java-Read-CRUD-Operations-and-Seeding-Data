f3991159b60b6cbad4125a3212dd11692a17741f
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-06 13:05:05 -0500

    Finished Menus Model

--- src/main/java/local/patrickchow0803/crudyrestaurants/models/Menu.java
+++ src/main/java/local/patrickchow0803/crudyrestaurants/models/Menu.java
@@ -15,4 +15,50 @@ public class Menu {
     private String dish;
     private double price;
 
+    @ManyToOne
+    @JoinColumn(name = "restaurantid",
+                nullable = false) //Use the restaurantid from the Restaurant Model Class
+    private Restaurant restaurant;
+
+    public Menu() {
+        // Spring Data JPA Requires this default constructor
+    }
+
+    public Menu(String dish, double price, Restaurant restaurant) {
+        this.dish = dish;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public long getMenuid() {
+        return menuid;
+    }
+
+    public void setMenuid(long menuid) {
+        this.menuid = menuid;
+    }
+
+    public String getDish() {
+        return dish;
+    }
+
+    public void setDish(String dish) {
+        this.dish = dish;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
 }
